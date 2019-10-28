fc0f84b60155b4879f0daec7c3ddaaad3bbf9b44
Jonathan Chuang <jochuang@umich.edu>
2019-10-27 22:24:21 -0400

    Intial commit

---
+++ rpn.py
@@ -0,0 +1,26 @@
+#!/bin/bash/env python3
+
+def calculate(arg):
+	stack = list()
+	for token in arg.split():
+		if token == '+':
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = arg1 + arg2
+			stack.append(result)
+		elif token == '-':
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = arg1 - arg2
+			stack.append(result)
+		else:
+			stack.append(int(token))
+		print(stack)
+	return stack.pop()
+
+def main():
+	while True:
+		calculate(input('rpn calc> '))
+
+if __name__ == '__main__':
+	main()
