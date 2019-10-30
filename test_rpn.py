dee13bc953d4d57a49b6c72e1fe24873ec2ecada
Jonathan Chuang <jochuang@umich.edu>
2019-10-30 11:46:05 -0400

    Added test cases for multiplication and division Added code in rpn.py for multiplication and division

--- test_rpn.py
+++ test_rpn.py
@@ -8,6 +8,12 @@ class TestBasics(unittest.TestCase):
 	def test_sub(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
+	def test_multi(self):
+		result = rpn.calculate('2 3 *')
+		self.assertEqual(6, result)
+	def test_divide(self):
+		result = rpn.calculate('6 3 /')
+		self.assertEqual(2, result)
 	def test_badinput(self):
 		with self.assertRaises(TypeError):
 			result = rpn.calculate('1 2 3 +')
