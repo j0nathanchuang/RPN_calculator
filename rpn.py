269f9b6399c1cbbf69ee18c962e5a7f4da8fea6f
Jonathan Chuang <jochuang@umich.edu>
2019-11-01 14:03:29 -0400

    Simplified code using hashtables
    Used try and except

--- rpn.py
+++ rpn.py
@@ -5,9 +5,43 @@ import sys
 def red(skk): print("\033[91m {}\033[00m" .format(skk)) #red for negatives
 def green(skk): print("\033[92m {}\033[00m" .format(skk)) #green for positives
 
+def add(a, b):
+	return a + b
+
+def sub(a, b):
+	return a - b
+
+def multi(a, b):
+	return a * b
+
+def divide(a, b):
+	return a / b
+
+def mod(a, b):
+	return a % b
+
+#hashtable
+operators = {
+	'+': add,
+	'-': sub,
+	'*': multi,
+	'/': divide,
+	'%': mod
+}
+
 def calculate(arg):
 	stack = list()
 	for token in arg.split():
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			function = operators[token]
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = function(arg1, arg2)
+			stack.append(result)
+		'''
 		if token == '+':
 			arg2 = stack.pop()
 			arg1 = stack.pop()
@@ -41,6 +75,7 @@ def calculate(arg):
 		else:
 			stack.append(int(token))
 		#print(stack)
+		'''
 	if len(stack) != 1:
 		raise TypeError
 	return stack.pop()
